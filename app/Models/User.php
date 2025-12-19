8008786c9c7919fd9aeb041cdce52b2dccd854ef
Nick-800 <sohaibkmsh@gmail.com>
2025-12-19 22:17:31 +0200

    Added and configured sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -46,3 +47,4 @@ class User extends Authenticatable
         ];
     }
 }
+
